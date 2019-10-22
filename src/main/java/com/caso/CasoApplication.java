28cebdb1c01dba669a899207d9ab8b824a5ff2b7
chicho <alphateam.sistemas@upeu.edu.pe>
2019-10-22 17:43:26 -0500

    base

---
+++ src/main/java/com/caso/CasoApplication.java
@@ -0,0 +1,13 @@
+package com.caso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caso/CasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
